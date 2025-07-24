80c25e97eec9e0cb5355c0b6032fb9367d254931
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2025-07-24 18:44:43 +0300

    Initial Commit

---
+++ RealDice/RealDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealDiceApp.swift
+//  RealDice
+//
+//  Created by Sedef Bozkurt on 24.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct RealDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
